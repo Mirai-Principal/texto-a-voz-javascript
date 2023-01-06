0cac1ffe42d09f875e96319c41a2496abcd5ec34
SecretBaseLab <46869971+SecretBaseLab@users.noreply.github.com>
2023-01-06 10:32:46 -0500

    se añadio texto a voz

--- script.js
+++ script.js
@@ -6,30 +6,23 @@ recognition.interimResults = false; //muestra alternativas
 recognition.lang = 'es-ES';         //lenguaje a escuchar
 recognition.maxAlternatives = 1;        //numero de alternativas
 
-recognition.onresult = async (e) => {
+recognition.onresult =  (e) => {
     let mensaje = '';
     //tomamos el ultimo mensaje
-    for (let i = e.resultIndex; i < e.results.length; i++) {
-        const result = e.results[i];
-        if (result.isFinal) {
-            mensaje += result[0].transcript
-            break
-        }
-         else {
-            mensaje += result[0].transcript;
-        }
-            
-        //valor de confianza del resultado
-        let confianza = e.results[0].confidence;
-    }
+    let ultimaRespuesta = e.resultIndex
+    const result = e.results[ultimaRespuesta];
 
+    let confianza  = e.results[0].confidence;
+    mensaje += result[0].transcript;
+
+    
     mensaje =  mensaje.trim()
     if(mensaje == "detener")
         recognition.stop()
     else{
         let file = "imagenes/" + mensaje + ".jpg" 
     
-        imagen.src = await file
+        imagen.src =  file
         imagen.onload = ()=>{
             console.log("carga correcta")
         }
@@ -61,4 +54,27 @@ recognition.onend = ()=>{
     textoMensaje.value = "";
     imagen.src = "./imagenes/intro.webp"
 
+    btnEscuchar.disabled = false;
+    btnDetener.disabled = true
+
+    texoAVoz("He dejado de oír")
+}
+
+recognition.onstart = ()=>{
+    btnEscuchar.disabled = true;
+    btnDetener.disabled = false
+
+    texoAVoz()
+}
+
+//texto a voz
+function texoAVoz(mensaje = "Estoy escuchando") {
+     var speech = new SpeechSynthesisUtterance();
+     speech.text = mensaje;
+     speech.volume = 1;
+     speech.rate = 1;
+     speech.pitch = 1;
+     speech.lang = "es-ES"
+ 
+     window.speechSynthesis.speak(speech);
 }
